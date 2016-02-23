21795a874af6ae3b5f38084ee6afa75240369a8e
steve_coffey <code@getbraintree.com>
2016-02-22 21:18:24 -0800

    sudo rm

--- release.sh
+++ release.sh
@@ -5,7 +5,7 @@ grunt
 
 git fetch
 git co $tag
-rm -rf /var/www/stevendcoffey/*
+sudo rm -rf /var/www/stevendcoffey/*
 sudo cp build/index.html /var/www/stevendcoffey/
 sudo cp -R build/js /var/www/stevendcoffey/
 sudo cp -R build/style /var/www/stevendcoffey/
