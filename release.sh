8b88beeb8a942b4af96581a8a034419e0f9a97e6
Steve Coffey <sdcoffey@umich.edu>
2016-12-21 00:44:50 -0800

    Update release script

--- release.sh
+++ release.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-gulp build --production
 
 export AWS_PROFILE=sdcoffey
 unset AWS_ACCESS_KEY_ID
 unset AWS_SECRET_KEY
 bucket_name=stevendcoffey.com
-tag=$1
+
+last_tag=`git tag --list | tail -n1`
+echo "Which tag to create (last tag: ${last_tag})"
+read tag
 
 if [ "$tag" == "" ]
 then
@@ -14,10 +16,16 @@ then
   exit 1
 fi
 
+git pull --tags
+git tag $tag -m $tag
 git checkout $tag
+
+gulp build --production
 
 aws s3 rm "s3://$bucket_name/" --recursive --region=us-west-1
 aws s3 cp ./build/prod/ "s3://$bucket_name" --recursive --region=us-west-1
+
+git push --tags
 
 git checkout master
 
