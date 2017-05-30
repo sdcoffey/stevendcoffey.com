2c40e4304c24c4901553d1b29372e78aea9f5410
sdcoffey <sdcoffey@umich.edu>
2017-05-30 09:32:13 -0700

    wip blog

--- config.rb
+++ config.rb
@@ -9,35 +9,23 @@ page '/*.xml', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
 
-# With alternative layout
-# page "/path/to/file.html", layout: :otherlayout
+activate :syntax
+set :markdown_engine, :redcarpet
+set :markdown, :fenced_code_blocks => true, :smartypants => true
 
-# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
-#  which_fake_page: "Rendering a fake page with a local variable" }
-
-# General configuration
-
-# Reload the browser automatically whenever files change
 configure :development do
-  activate :livereload
+	activate :livereload
 end
 
-###
-# Helpers
-###
+activate :blog do |blog|
+	blog.prefix = "words"
+	blog.layout = "blog_layout"
+end
 
-# Methods defined in the helpers block are available in templates
-# helpers do
-#   def some_helper
-#     "Helping"
-#   end
-# end
+helpers do
+end
 
-# Build-specific configuration
 configure :build do
-  # Minify CSS on build
-   activate :minify_css
-  # Minify Javascript on build
-   activate :minify_javascript
+	activate :minify_css
+	activate :minify_javascript
 end
