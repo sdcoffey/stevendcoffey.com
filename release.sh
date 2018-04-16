06214de7dc497052385b1845fdba2f30ecdd4f46
sdcoffey <sdcoffey@umich.edu>
2018-04-15 22:50:06 -0700

    Update release script

--- release.sh
+++ release.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-export AWS_PROFILE=sdcoffey
-unset AWS_ACCESS_KEY_ID
-unset AWS_SECRET_KEY
 bucket_name=stevendcoffey.com
 
 last_tag=`git tag --list | gsort -V | tail -n1`
@@ -25,7 +22,7 @@ aws s3 rm "s3://$bucket_name/" --recursive --region=us-west-1
 aws s3 cp ./build/ "s3://$bucket_name" --recursive --region=us-west-1
 
 aws configure set preview.cloudfront true
-aws cloudfront create-invalidation --distribution-id $stevendcoffey_distro_id --paths "/*"
+aws cloudfront create-invalidation --distribution-id $WEBSITE_DISTRO_ID --paths "/*"
 
 git push --tags
 
