ad7fd8f0aae18ce65eee29b36bfa86c8cbd46eb7
sdcoffey <stevendcoffey@gmail.com>
2019-03-31 20:11:53 -0700

    Add clock to toolbar

--- web/src/components/system/SystemToolbar.tsx
+++ web/src/components/system/SystemToolbar.tsx
@@ -1,24 +1,64 @@
 import * as React from "react";
+import classNames from "classnames";
+import moment from "moment";
 
 import { Apple } from "../shared/icons";
 import { BlurView } from "../shared/BlurView";
 
 import "../../style/SystemToolbar.scss";
 
-export default function SystemToolbar() {
-  return (
-    <BlurView
-      className="SystemToolbar"
-      childrenClassName="SystemToolbar--childWrapper"
-    >
-      <SystemToolbarItem>
-        <Apple height={20} width={20} />
-      </SystemToolbarItem>
-    </BlurView>
-  );
+interface SystemToolbarProps {}
+interface SystemToolbarState {
+  currentTime: moment.Moment;
+}
+
+export default class SystemToolbar extends React.Component<
+  SystemToolbarProps,
+  SystemToolbarState
+> {
+  timerToken: number = 0;
+
+  state = {
+    currentTime: moment()
+  };
+
+  componentDidMount() {
+    this.timerToken = window.setInterval(this._updateTime, 1000);
+  }
+
+  componentWillUnmount() {
+    window.clearInterval(this.timerToken);
+  }
+
+  render() {
+    const { currentTime } = this.state;
+
+    return (
+      <BlurView
+        className="SystemToolbar"
+        childrenClassName="SystemToolbar--childWrapper"
+      >
+        <div className="SystemToolbar--menuDock">
+          <SystemToolbarItem>
+            <Apple height={20} width={20} />
+          </SystemToolbarItem>
+        </div>
+        <div className="SystemToolbar--toolbarDock">
+          <SystemToolbarItem className="SystemToolbar--clock">
+            <div>{currentTime.format("ddd H:mm:ss")}</div>
+          </SystemToolbarItem>
+        </div>
+      </BlurView>
+    );
+  }
+
+  _updateTime = () => {
+    this.setState({ currentTime: moment() });
+  };
 }
 
 interface SystemToolbarItemProps {
+  className?: string;
   children: React.ReactNode;
 }
 
@@ -32,11 +72,14 @@ class SystemToolbarItem extends React.Component<SystemToolbarItemProps> {
   };
 
   render() {
-    const { children } = this.props;
+    const { children, className } = this.props;
     const { active } = this.state;
 
     return (
-      <div onClick={this.handleClick} className="SystemToolbarItem">
+      <div
+        onClick={this.handleClick}
+        className={classNames("SystemToolbarItem", className)}
+      >
         {children}
       </div>
     );
